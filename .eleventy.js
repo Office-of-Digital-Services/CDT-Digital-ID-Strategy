e3a78184bd0fdaeacdc8da06b68a425d60420eec
mark-ohanesian <Mark.Ohanesian@state.ca.gov>
2025-11-19 21:27:06 -0800

    Update eleventy config to passthrough copy images to build to fix broken media and links

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
 module.exports = function (eleventyConfig) {
+  // Passthrough copy for static assets
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("css");
+  // Add more passthroughs if needed (e.g., fonts, js)
   return {
     dir: {
       input: ".",
