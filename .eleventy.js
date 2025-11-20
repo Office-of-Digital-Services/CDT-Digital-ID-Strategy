dd2ba5359e40fcff4d04e9fb2eef1e21eca13cc6
mark-ohanesian <Mark.Ohanesian@state.ca.gov>
2025-11-20 12:50:08 -0800

    Add config to pass through docs and root utility files

--- .eleventy.js
+++ .eleventy.js
@@ -2,14 +2,20 @@ module.exports = function (eleventyConfig) {
   // Passthrough copy for static assets
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy({
+    "src/root": "."
+  });
+  eleventyConfig.addPassthroughCopy({
+    "src/docs": "."
+  });
   // Add more passthroughs if needed (e.g., fonts, js)
   return {
     dir: {
       input: "src",
       includes: "_includes",
-      output: "_site",
+      output: "_site"
     },
     htmlTemplateEngine: "njk",
-    templateFormats: ["html", "njk"],
+    templateFormats: ["html", "njk"]
   };
 };
