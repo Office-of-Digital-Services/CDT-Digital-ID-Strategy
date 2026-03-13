a2d25b3c511724e7d7614672520340649833594a
Carter Medlin <carterm@users.noreply.github.com>
2026-03-13 11:52:50 -0700

    fixing nesting

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -3,6 +3,8 @@ export default {
   extends: ["stylelint-config-standard"],
   rules: {
     "selector-class-pattern": null,
+    "nesting-selector-no-missing-scoping-root": true,
+    "color-no-invalid-hex": true,
     "selector-nested-pattern": [
       "^(?:&(?!\\s*>)|[^a-zA-Z0-9])",
       {
